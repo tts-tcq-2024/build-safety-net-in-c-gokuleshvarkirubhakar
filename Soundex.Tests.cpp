0fe927b63272e9a2d57b6a07a594df438e32601c
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-04 16:48:31 +0530

    Update test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -25,8 +25,58 @@ TEST(SoudexTestsuite, SingleCharacter) {
   EXPECT_STREQ(soundex,"Z000");
 }
 
+TEST(SoudexTestsuite, MaximumCharacters) {
+  char soundex[5];
+  generateSoundex("abcdefghijklmnopqrstuvwxyzab", soundex);
+  EXPECT_STREQ(soundex,"A123");
+}
+
 TEST(SoudexTestsuite, AllVowels) {
   char soundex[5];
   generateSoundex("AEIOU", soundex);
   EXPECT_STREQ(soundex,"A000");
+}
+
+TEST(SoudexTestsuite, NonMappableCharacters) {
+  char soundex[5];
+  generateSoundex("147!@#$", soundex);
+  EXPECT_STREQ(soundex,"1000");
+}
+
+TEST(SoudexTestsuite, MixCharacters) {
+  char soundex[5];
+  generateSoundex("P1Q2R2S3T4", soundex);
+  EXPECT_STREQ(soundex,"P262");
+}
+
+TEST(SoudexTestsuite, Normal) {
+  char soundex[5];
+  generateSoundex("CLEANCODE", soundex);
+  EXPECT_STREQ(soundex,"C452");
+}
+
+TEST(SoudexTestsuite, NormalMixedCase) {
+  char soundex[5];
+  generateSoundex("ClEaNcOdE", soundex);
+  EXPECT_STREQ(soundex,"C452");
+}
+
+TEST(SoudexTestsuite, SameMappingCharacters) {
+  char soundex[5];
+  generateSoundex("ZBFCGDTLMNR", soundex);
+  EXPECT_STREQ(soundex,"Z123");
+}
+
+TEST(SoudexTestsuite, VowelSeparation) {
+  char soundex[5];
+  generateSoundex("Roses", soundex);
+  EXPECT_STREQ(soundex,"R220");
+}
+
+TEST(SoudexTestsuite, HorWSeparation) {
+  char soundex[5];
+  generateSoundex("Burroughs", soundex);
+  EXPECT_STREQ(soundex,"B620");
+  generateSoundex("MIJWKTS", soundex);
+  EXPECT_STREQ(soundex,"M232");
 }
