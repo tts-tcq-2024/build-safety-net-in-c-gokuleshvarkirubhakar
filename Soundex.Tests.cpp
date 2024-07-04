6b922a4ac85b12b84803603bb1d730596ccd3524
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-04 10:43:19 +0530

    Updated initial set of test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,32 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
+TEST(SoudexTestsuite, InvalidCharactersSpCharSet1) {
   char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+  generateSoundex("A$SAM", soundex);
+  EXPECT_STREQ(soundex,"A250");
+}
+
+TEST(SoudexTestsuite, InvalidCharactersSpCharSet2) {
+  char soundex[5];
+  generateSoundex("N^GALAND", soundex);
+  EXPECT_STREQ(soundex,"N245");
+}
+
+TEST(SoudexTestsuite, EmptyCharacter) {
+  char soundex[5];
+  generateSoundex("", soundex);
+  EXPECT_STREQ(soundex,"");
+}
+
+TEST(SoudexTestsuite, SingleCharacter) {
+  char soundex[5];
+  generateSoundex("Z", soundex);
+  EXPECT_STREQ(soundex,"Z000");
+}
+
+TEST(SoudexTestsuite, AllVowels) {
+  char soundex[5];
+  generateSoundex("AEIOU", soundex);
+  EXPECT_STREQ(soundex,"A000");
 }
